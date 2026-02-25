da8e0c67a462ac7d0fe8419f86bea4a7b488e4fc
OmerAhmedQuadri <omerquadri953@gmail.com>
2026-02-25 10:58:07 +0530

    25-02-2026

--- test.js
+++ test.js
@@ -1,22 +1,12 @@
-
-// user = {
-//     name: 'omer',
-//     city: 'Hyderabad',
-//     age: 21
-// }
-// const address = {
-//     door: 15,
-//     state: 'ts'
-// }
-// // const {name, ...items} = {user}
-// const newUser = {...user, ...address}
-// const {name, ...items} = newUser
-
-// console.log(newUser)
-// console.log(name, items)
-
-const add = (...items) => {
-    console.log( items)
+let user = {
+    name: 'omer',
+    age: 21
 }
 
-add('omer', 1, 2, 3, 4, 5)
+let userDetails = {
+    age: 22,
+    city: 'hyderabad'
+}
+
+user = { ...userDetails, ...user}
+console.log(user);
