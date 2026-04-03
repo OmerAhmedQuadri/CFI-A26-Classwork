dcde156a967fcf5c15ba8b4c33d29e8bb14f2f1d
OmerAhmedQuadri <omerquadri953@gmail.com>
2026-04-03 12:49:58 +0530

    03-04-2026 session-1

---
+++ Phase-2/10-dom/02/script.js
@@ -0,0 +1,6 @@
+const HOURS_DISPLAY = document.getElementById('hours')
+const MINUTES_DISPLAY = document.getElementById('minutes')
+const SECONDS_DISPLAY = document.getElementById('seconds')
+
+const START_STOP_BTN = document.getElementById('start-stop-btn')
+const RESET_BTN = document.getElementById('reset-btn')
