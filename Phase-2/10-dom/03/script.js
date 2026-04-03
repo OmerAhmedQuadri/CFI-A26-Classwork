a64c1851d5eb914d7c1fc394e2bf4df76ae5b381
OmerAhmedQuadri <omerquadri953@gmail.com>
2026-04-03 16:49:14 +0530

    03-04-2026 session-2

--- Phase-2/10-dom/03/script.js
+++ Phase-2/10-dom/03/script.js
@@ -1,40 +1,48 @@
 const CODE_DISPLAY = document.getElementById('code-txt')
 const redBtn = document.getElementById('red-btn')
-// const blueBtn = document.getElementById('blue-btn')
-// const greenBtn = document.getElementById('green-btn')
+const blueBtn = document.getElementById('blue-btn')
+const greenBtn = document.getElementById('green-btn')
 const statusBtn = document.getElementById('status-btn')
+const setColours = document.getElementById('set-colours')
+let isEnabled = true
 
-let counter = 0
-redBtn.addEventListener('click', updateColour)
-// blueBtn.addEventListener('click', updateColour)
-// greenBtn.addEventListener('click', updateColour)
-statusBtn.addEventListener('click', disableBtns)
+CODE_DISPLAY.style.color = localStorage.getItem('colour')
 
-function updateColour() {
-    CODE_DISPLAY.style.color = 'red'
-    console.log('udpate colour was executed '+ counter++);
+redBtn.addEventListener('click', () => {
+    if (isEnabled) {
+        CODE_DISPLAY.style.color = 'red'
+        localStorage.setItem('colour', 'red')
+    }
+})
+blueBtn.addEventListener('click', () => {
+    if (isEnabled) {
+        CODE_DISPLAY.style.color = 'blue'
+        localStorage.setItem('colour', 'blue')
+    }
+})
+greenBtn.addEventListener('click', () => {
+    if (isEnabled) {
+        CODE_DISPLAY.style.color = 'green'
+        localStorage.setItem('colour', 'green')
+    }
+})
 
-    // return () => { console.log('returned function is executed');}
-}
+statusBtn.addEventListener('click', () => {
+    if(isEnabled){
+        isEnabled = false
+        statusBtn.innerText = 'Enable'
+        CODE_DISPLAY.style.color = 'yellow'
 
-
-function disableBtns(){
-    console.log('disableBtns was called');
-    redBtn.removeEventListener('click', updateColour)
-    redBtn.style.backgroundColor = 'rgb(255, 145, 145)'
-    CODE_DISPLAY.style.color = 'black'
-    statusBtn.innerText = 'Enable'
-    // statusBtn.removeEventListener('click', disableBtns)
-    // statusBtn.addEventListener('click', enableBtns)
-}
-
-function enableBtns(){
-    console.log('enableBtns was called');
-
-    redBtn.addEventListener('click', updateColour)
-    CODE_DISPLAY.style.color = 'yellow'
-    statusBtn.innerText = 'Disable'
-    statusBtn.addEventListener('click', disableBtns)
-    statusBtn.removeEventListener('click', enableBtns)
-}
-
+        redBtn.classList.add('disabled')
+        blueBtn.classList.add('disabled')
+        greenBtn.classList.add('disabled')
+    }
+    else {
+        isEnabled = true
+        statusBtn.innerText = 'Disable'
+        
+        redBtn.classList.remove('disabled')
+        blueBtn.classList.remove('disabled')
+        greenBtn.classList.remove('disabled')
+    }
+})
